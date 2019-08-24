c0b9a6922258e037424919e54d8919182262249b
Bogdan <bogdanguk@gmail.com>
2019-08-25 01:03:13 +0300

    created interface and set some IBOutlets and IBActions

--- TapMeFast/ViewController.swift
+++ TapMeFast/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    var timer = Timer()
+    
+    var timeInt = 10
+    var scoreInt = 0
+    var gameInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func startGame(_ sender: Any) {
+        
+    }
+    
 }
 
