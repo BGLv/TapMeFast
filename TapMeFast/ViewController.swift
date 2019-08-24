09928bfdb8cc31f94a2a17e1b2f6c6a998a931f2
Bogdan <bogdanguk@gmail.com>
2019-08-25 01:30:02 +0300

    created competitive reaction based game

--- TapMeFast/ViewController.swift
+++ TapMeFast/ViewController.swift
@@ -23,11 +23,63 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timeInt = 10
     }
 
     
     @IBAction func startGame(_ sender: Any) {
         
+        if timeInt == 10 {
+        
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startCounter), userInfo: nil, repeats: true)
+            
+            startGameButton.isEnabled = false
+            startGameButton.alpha = 0.5
+        }
+        if gameInt == 1 {
+            
+            scoreInt += 1
+            scoreLabel.text = String(scoreInt)
+            
+        } else {
+            timeInt = 10
+            scoreInt = 0
+            timeLabel.text = String(timeInt)
+            scoreLabel.text = String(scoreInt)
+            startGameButton.setTitle("Start", for: UIControl.State.normal)
+        }
+        
+    }
+    
+    @objc func startCounter(){
+        
+        timeInt -= 1
+        
+        timeLabel.text = String(timeInt)
+        
+        startGameButton.isEnabled = true
+        startGameButton.alpha = 1
+        
+        startGameButton.setTitle("Tap", for: UIControl.State.normal)
+        
+        gameInt = 1
+        
+        if timeInt == 0 {
+            timer.invalidate()
+            startGameButton.isEnabled = false
+            startGameButton.alpha = 0.5
+            
+            startGameButton.setTitle("Restart", for: UIControl.State.normal)
+        
+            timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(restart), userInfo: nil, repeats: false)
+        }
+        
+    }
+    
+    @objc func restart(){
+        gameInt = 0
+        startGameButton.isEnabled = true
+        startGameButton.alpha = 1
     }
     
 }
